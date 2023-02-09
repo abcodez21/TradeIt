2b758241c4f3f82ae600b62ed270f77f8e6b9f15
Abdallahi <abcodez21@outlook.com>
2023-02-08 19:22:57 -0500

    Initial Commit

---
+++ TradeIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TradeIt
+//
+//  Created by Abdallahi Thiaw on 2/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
